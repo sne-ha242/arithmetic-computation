092c70346db29c97234b907f8ce42fa7516171a9
sneha wandhare <snehawandhare101996@gmail.com>
2021-01-15 01:55:37 +0530

    Added: Arithmetic computation and sorting uc6

---
+++ computeuc6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+read -p "Enter the number: " a
+read -p "Enter the number: " b
+read -p "Enter the number: " c
+
+declare -A result
+result[x]=$(( $a + $b * $c ))
+result[y]=$(( $a * $b + $c ))
+result[z]=$(( $c % $b + $a ))
+
+echo "result of x computation: " ${result[x]}
+echo "result of y computation: " ${result[y]}
+echo "result of z computation: " ${result[z]}
+echo "result of all computation: " ${result[@]}
